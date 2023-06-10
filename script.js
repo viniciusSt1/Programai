9d65b7a1921a766651874fc1eaa2c98e38a98fde
Vinicius Stefanes <stefanesv31@gmail.com>
2023-06-10 02:42:47 -0300

    Postando site - lista 1 completa

---
+++ script.js
@@ -0,0 +1,32 @@
+function scrollTo(element, to, duration) {
+    if (duration <= 0) return;
+    var difference = to - element.scrollTop;
+    var perTick = difference / duration * 10;
+    
+    setTimeout(function() {
+      element.scrollTop = element.scrollTop + perTick;
+      if (element.scrollTop === to) return;
+      scrollTo(element, to, duration - 10);
+    }, 10);
+  }
+  
+  document.querySelectorAll('a.smoth-scroll').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      var targetElement = document.querySelector(this.getAttribute('href'));
+      scrollTo(document.documentElement, targetElement.offsetTop, 1000);
+    });
+  });
+
+  function showhidden(element){
+    div=document.getElementById(element);
+    if(div.className == "dica hidden")
+        div.className = "dica show"
+    else if(div.className == "dica show")
+            div.className = "dica hidden"
+        else if(div.className == "resposta hidden")
+                div.className="resposta show"
+            else
+                div.className="resposta hidden"
+  }
