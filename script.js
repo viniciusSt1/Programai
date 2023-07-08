e885c001d1d049eb58914e45fcede91dda755b72
Vinicius Stefanes <stefanesv31@gmail.com>
2023-07-08 20:51:02 -0300

    Header responsivo, logotipo, favicon, organização de arquivos e adições na seção do autor

--- script.js
+++ script.js
@@ -19,14 +19,26 @@ function scrollTo(element, to, duration) {
     });
   });
 
-  function showhidden(element){
-    div=document.getElementById(element);
-    if(div.className == "dica hidden")
-        div.className = "dica show"
-    else if(div.className == "dica show")
-            div.className = "dica hidden"
+function showhidden(element){
+  div=document.getElementById(element);
+  if(div.className == "dica hidden")
+      div.className = "dica show"
+  else if(div.className == "dica show")
+          div.className = "dica hidden"
         else if(div.className == "resposta hidden")
                 div.className="resposta show"
             else
                 div.className="resposta hidden"
+}
+
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").classList.remove("hiddenHeader");
+  } else {
+    document.getElementById("header").classList.add("hiddenHeader");
   }
+  prevScrollpos = currentScrollPos;
+};
